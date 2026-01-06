e19065d382124af207bc470ad6eafcea45b9f6ce
Jorge Calderón <jcalderon900610@gmail.com>
2026-01-05 23:14:38 -0500

    UPDATE

--- src/routes/transcription.routes.js
+++ src/routes/transcription.routes.js
@@ -2,11 +2,22 @@ import { Router } from "express";
 import {
     createTranscription,
     getTranscriptionsByUser,
+    generateTranscriptionAnalysis,
+    getTranscriptionAnalysis,
+    updateTranscriptionCheckedSteps,
 } from "../controllers/transcription.controller.js";
 
 const router = Router();
 
 router.post("/transcription", createTranscription);
 router.get("/transcription/user/:userId", getTranscriptionsByUser);
+
+// Analysis routes
+router.post("/generate-analysis", generateTranscriptionAnalysis);
+router.get("/analysis/:transcription_id", getTranscriptionAnalysis);
+router.put(
+    "/analysis/:transcription_id/steps",
+    updateTranscriptionCheckedSteps
+);
 
 export default router;
