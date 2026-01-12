8641cb0f915c0eccc68124bed869fe8b7733ff0c
Jorge Calderón <jcalderon900610@gmail.com>
2026-01-11 23:11:04 -0500

    upd: PRODUCCION

--- src/routes/strategy.routes.js
+++ src/routes/strategy.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { get_user_strategies } from "../controllers/strategy.controller.js";
-import { auth_required } from "../middlewares/validate_token.js";
+import {
+    auth_required,
+    admin_required,
+} from "../middlewares/validate_token.js";
 
 const strategy_router = Router();
 
-strategy_router.get("/get", auth_required, get_user_strategies);
+// Admin only - M&A Pipeline access
+strategy_router.get("/get", auth_required, admin_required, get_user_strategies);
 
 export default strategy_router;
